787f4a0ded56edb1ce219305a972d9f8e28f27b6
NandhanaRameshkumar <nandhu2374@gmail.com>
2025-05-24 14:07:33 +0530

    Initial commit

---
+++ Q1/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import StockPage from "./components/StockPage";
+import CorrelationHeatmap from "./components/CorrelationHeatmap";
+
+function App() {
+    const dummyStockData = {
+        AAPL: [150, 152, 153, 155, 154],
+        MSFT: [300, 302, 301, 303, 304],
+        GOOGL: [2700, 2710, 2720, 2730, 2740]
+    };
+
+    return (
+        <div>
+            <h1>React Stock Aggregator</h1>
+            <StockPage />
+            <CorrelationHeatmap stockData={dummyStockData} />
+        </div>
+    );
+}
+
+export default App;
